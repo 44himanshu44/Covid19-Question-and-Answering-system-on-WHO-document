6fe75d5df883365909477b4b6617a32ff6e3246c
44himanshu44 <himanshutalegaonkar6@gmail.com>
2020-11-04 13:14:04 +0530

    QnA

---
+++ main.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import re
+from allennlp.predictors.predictor import Predictor
+from flask import Flask, render_template, request
+app = Flask(__name__)
+predictor_18 = Predictor.from_path('https://storage.googleapis.com/allennlp-public-models/bidaf-elmo-model-2018.11.30-charpad.tar.gz')
+predictor_17 = Predictor.from_path('https://storage.googleapis.com/allennlp-public-models/bidaf-model-2017.09.15-charpad-allennlpv1.0.tar.gz')
+
+f = open('corona_new.txt','r')
+text = f.read()
+text = re.sub('\n','',text)
+text = text.lower()
+
+@app.route('/')
+def index():
+    return render_template('index_2.html')
+
+
+@app.route('/QA_page')
+def QA_page():
+    return render_template('index.html')
+
+@app.route('/main_page')
+def main_page():
+    question = request.args.get('topic')
+    #question = request.args.get('question')
+    answer = qna(text, question)
+    return render_template('index.html', answer = answer)
+
+def qna(text, question):
+    context = text
+    result_18 = predictor_18.predict(passage = context, question = question)
+    result_17 = predictor_17.predict(passage = context, question = question)
+    
+    if len(result_18) >= len(result_18):
+        return result_18['best_span_str']
+    else:
+        return result_17['best_span_str']
+
+
+
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+#app.run(host = '10.26.34.190', port = 9100, debug = True)
