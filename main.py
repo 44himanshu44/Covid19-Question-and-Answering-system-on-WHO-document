9f59429a8f90281d335e6b49dbdf9424c4ec1314
Himanshu Talegaonkar <53419904+44himanshu44@users.noreply.github.com>
2020-11-05 14:07:36 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import re
 from allennlp.predictors.predictor import Predictor
+import allennlp_models.rc
 from flask import Flask, render_template, request
 app = Flask(__name__)
-predictor_18 = Predictor.from_path('https://storage.googleapis.com/allennlp-public-models/bidaf-elmo-model-2018.11.30-charpad.tar.gz')
-predictor_17 = Predictor.from_path('https://storage.googleapis.com/allennlp-public-models/bidaf-model-2017.09.15-charpad-allennlpv1.0.tar.gz')
+
+
+predictor_20 = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/bidaf-elmo-model-2020.03.19.tar.gz")
+
+
+#predictor_18 = Predictor.from_path('bidaf-elmo-model-2018.11.30-charpad.tar.gz')
+#predictor_17 = Predictor.from_path('bidaf-model-2017.09.15-charpad-allennlpv1.0.tar.gz')
 
 f = open('corona_new.txt','r')
 text = f.read()
@@ -29,13 +35,14 @@ def main_page():
 
 def qna(text, question):
     context = text
-    result_18 = predictor_18.predict(passage = context, question = question)
-    result_17 = predictor_17.predict(passage = context, question = question)
-    
-    if len(result_18) >= len(result_18):
-        return result_18['best_span_str']
-    else:
-        return result_17['best_span_str']
+    result_20 = predictor_20.predict(passage = context, question = question)
+    #result_17 = predictor_17.predict(passage = context, question = question)
+
+    #if len(result_18) >= len(result_18):
+     #   return result_18['best_span_str']
+    #else:
+     #   return result_17['best_span_str']
+    return result_20['best_span_str']
 
 
 
